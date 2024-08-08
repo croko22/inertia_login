9bfcfaa56c364ca83120461deab8a149c96e3607
Kevin <kvchambit@gmail.com>
2024-08-07 22:27:57 -0500

    feat: Api login with sanctum

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'The provided credentials are incorrect.'
+            ], Response::HTTP_UNAUTHORIZED);
+
+        }
+
+        return response()->json([
+            'data' => [
+                'attributes' => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email
+                ],
+                'token' => $user->createToken($request->device_name)->plainTextToken
+            ],
+        ], Response::HTTP_OK);
+    }
+
+    public function hello(): JsonResponse
+    {
+        return response()->json(['message' => 'Hello World!']);
+    }
+}
